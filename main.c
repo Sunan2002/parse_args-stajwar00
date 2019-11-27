a415712c41a2b6d183e27045e9d702dcadc8f358
Sunan Tajwar <sunantajwar@Sunans-MBP.home>
2019-11-26 21:20:00 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+char ** parse_args( char * line ){
+  char ** line = calloc(5, sizeof(*char));
+  int index = 0;
+  char * sep = line;
+  while (strsep(
+
+
+}
+
+
+int main(int argc. char *argv[]){
+  
+  /*
+  char line[100] = "woah-this-is-cool";
+  char *s1 = line;
+  printf ("[%s]\n", strsep(&s1, "-"));
+  printf("[%s]\n", s1);
+  return 0;
+  */
+
+  
+}
+
+    
